29d5945d9a33b542134b87db04de35af553d0ce5
Mads Hartmann <mads379@gmail.com>
2011-03-14 10:54:49 +0100

    TreeSet is now a Traversable. Added test methods

--- src/TreeSet.coffee
+++ src/TreeSet.coffee
@@ -11,7 +11,9 @@ if require?
   Traversable = (require './Traversable').Traversable
 
 ###*
-  
+  @augments mahj.Traversable
+  @class TreeSet provides the implementation of the abstract data type Set based on a Red Black Tree 
+  @public
 ###  
 TreeSet = (elements,comparator) ->
   if elements instanceof Array and elements.length > 0 
@@ -24,23 +26,36 @@ TreeSet = (elements,comparator) ->
   this.tree.comparator = comparator if comparator?
   this
 
+TreeSet.prototype = new mahj.Traversable()
+
 ###*
-  
+  Insert an element in the set. If the set already contains an element equal to the given value, 
+  it is replaced with the new value.
+  @param element The element to insert into the set 
 ###
 TreeSet.prototype.insert = ( element ) -> 
   this.buildFromTree(this.tree.insert(element,element))
 
 ###*
-  
+  Delete an element from the set
+  @param element The element to remove from the set
 ###
 TreeSet.prototype.remove = ( element ) ->
   this.buildFromTree(this.tree.remove(element))
 
 ###*
-  
+  Tests if the set contains the element
+  @param element The element to check for 
 ###
 TreeSet.prototype.contains = ( element ) -> 
   this.tree.containsKey( element )
+
+###*
+  The elements of the set
+  @return {List} A list containing all the element of the set
+###
+TreeSet.prototype.values = () -> 
+  this.tree.values()
 
 ###*
   Used to construct a TreeMap from RedBlackTree. This is intended 
@@ -67,10 +82,12 @@ TreeSet.prototype.buildFromArray = (arr) ->
 ###*
   Applies function 'f' on each value in the map. This return nothing and is only invoked
   for the side-effects of f. 
+  @param f The unary function to apply on each element in the set.
   @see mahj.Traversable
 ###
 TreeSet.prototype.forEach = ( f ) -> 
-  this.tree.inorderTraversal( f )
+  q = (kv) -> { key: f(kv.key), value: f(kv.value) }
+  this.tree.inorderTraversal( q )
 
 if exports?
   exports.TreeSet = TreeSet
--- test/TreeSet.js
+++ test/TreeSet.js
@@ -9,17 +9,73 @@ $(document).ready(function(){
   
   test("It's possible to check if the set contains a specific element",function() {
     var set = new TreeSet([1,2,3,4,5]);
-    ok(set.contains(3) === true);
+    ok(set.contains(3));
   });
   
   test("It's possible to insert a new element to the set",function() {
     var set = new TreeSet([1,2,3,4,5]).insert(6);
-    ok(set.contains(6) === true);
+    ok(set.contains(6));
   });
   
   test("It's possible to remove an element from the set",function() {
     var set = new TreeSet([1,2,3,4,5]).remove(3);
-    ok(set.contains(3) === false);
+    ok(!set.contains(3));
+  });
+  
+  test("When inserting an element that already exists it shouldn't be duplicated", function() {
+    var set = new TreeSet([1,2,3,4,5]).insert(5);
+    ok(set.contains(5));
+
+    var values = set.values();
+    ok(!values.get(0).isEmpty());
+    ok(!values.get(1).isEmpty());
+    ok(!values.get(2).isEmpty());
+    ok(!values.get(3).isEmpty());
+    ok(!values.get(4).isEmpty());
+    ok(values.get(6).isEmpty());
+  });
+  
+  module("TreeSet - Traversable methods");
+  
+  test("You can map a TreeSet to another TreeSet", function() {
+    var set = new TreeSet([1,2,3,4]).map( function(elem){ return elem*2; }),
+        shouldbeSet = new TreeSet([2,4,6,8]),
+        list = set.values(),
+        shouldbe = shouldbeSet.values();
+            
+    ok( list.get(0).get() == shouldbe.get(0).get() );    
+    ok( list.get(1).get() == shouldbe.get(1).get() );    
+    ok( list.get(2).get() == shouldbe.get(2).get() );    
+    ok( list.get(3).get() == shouldbe.get(3).get() );    
+  });
+  
+  test("You can flatMap a TreeSet to another TreeSet", function() {
+    var set = new TreeSet([1,3]).flatMap( function(elem){ 
+          return new TreeSet([elem,elem+1]);
+        }),
+        shouldbeSet = new TreeSet([1,2,3,4]),
+        list = set.values(),
+        shouldbe = shouldbeSet.values(); 
+    
+    ok( list.get(0).get() == shouldbe.get(0).get() );    
+    ok( list.get(1).get() == shouldbe.get(1).get() );    
+    ok( list.get(2).get() == shouldbe.get(2).get() );    
+    ok( list.get(3).get() == shouldbe.get(3).get() );
+  });
+
+  test("You can filter a TreeSet", function() {
+    var set = new TreeSet([1,2,3,4,5,6,7,8,9,10]).filter( function(elem){ 
+          return elem >= 5; 
+        }),
+        list = set.values();  
+    
+    
+    ok( list.get(0).get() == 5 );    
+    ok( list.get(1).get() == 6 );    
+    ok( list.get(2).get() == 7 );
+    ok( list.get(3).get() == 8 );
+    ok( list.get(4).get() == 9 );
+    ok( list.get(5).get() == 10 );
   });
   
 });
