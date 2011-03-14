e50e2d00516c8a56aba4b4282b4cb9474427493a
Mads Hartmann <mads379@gmail.com>
2011-03-14 14:07:48 +0100

    Implemented a Queue based on two lists. Also wrote tests

---
+++ test/Queue.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  
+  module("Queue");
+  
+  test("It's possible to create a queue with multiple elements",function() {
+    var queue = new Queue(1,2,3,4,5);
+    ok(queue !== undefined);
+  });
+  
+  test("It's possible to read the front element in the queue",function() {
+    var queue = new Queue(1,2,3,4,5);
+    ok(queue.front() === 1);
+  });
+  
+  test("It's possible to get a List with all of the elements in the Queue",function() {
+    var queue = new Queue(1,2,3),
+        list = queue.values();
+        
+    ok(list.get(0).get() === 1);
+    ok(list.get(1).get() === 2);
+    ok(list.get(2).get() === 3);
+  });
+  
+  test("It's possible to dequeue an element from the queue",function() {
+    var queue = new Queue(1,2,3,4,5,6).dequeue(),
+        list = queue.values();
+    
+    ok(list.get(0).get() === 2);
+    ok(list.get(5).isEmpty());
+  });
+  
+});
